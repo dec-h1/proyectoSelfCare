d8b5bd9519689bbaca380b3cca9c381030059696
Andre Iparraguirre <andreiparraguirre@hotmail.com>
2021-12-02 12:40:12 -0500

    pagina login creada

---
+++ src/pages/login/components/formlogin.jsx
@@ -0,0 +1,46 @@
+import { Form, Input, Button} from 'antd';
+
+function FormLogin(){
+    return(
+        <Form
+      name="basic"
+      labelCol={{span: 9}}
+      initialValues={{ remember: true }}
+      autoComplete="off"
+    >
+      <Form.Item className="grid grid-cols-1 px-5 justify-items-start"
+        label="Email    "
+        name="email"
+        rules={[{ required: true, message: 'Por favor, Ingresa tu correo' }]}
+      >
+        <Input  className="bg-transparent border-t-0 border-r-0 border-l-0 font-bold hover:bg-transparent hover:border-yellow-400 hover:border-t-0 hover:border-r-0 hover:border-l-0"/>
+      </Form.Item>
+
+      <Form.Item className="grid grid-cols-1 px-5"
+        label="Contraseña"
+        name="password"
+        rules={[{ required: true, message: 'Por favor, Ingresa una contraseña' }]}
+      >
+        <Input.Password className="bg-transparent border-t-0 border-r-0 border-l-0 font-bold hover:bg-transparent hover:border-yellow-400 hover:border-t-0 hover:border-r-0 hover:border-l-0"/>
+      </Form.Item>
+
+      <div className="text-right font-bold">
+          <span>¿Olvidaste la contraseña?</span>
+      </div>
+
+
+      <Form.Item className="px-5 pt-10"
+      wrapperCol={{span: 24 }}>
+        <Button type="primary" htmlType="submit" className="bg-yellow-300 rounded-xl border-yellow-300 text-white w-full h-12 font-bold hover:bg-yellow-400 hover:border-yellow-400 hover:text-black">
+          Iniciar Sesión   
+        </Button>
+      </Form.Item>
+
+      <div className="text-center font-bold">
+          <label>¿No tienes una cuenta?</label> <span className="text-yellow-300">Registrarse</span>
+      </div>
+    </Form>
+    );
+}
+
+export{FormLogin};
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,16 @@
 import { TitlePage } from "../../components/title-page";
+import { FormLogin } from "../login/components/formlogin";
 
 function PageLogin() {
   return (
     <div className="page-login flex flex-col h-screen">
       <TitlePage
-        title="Inicie Sesion"
-        suptitle="Ingrese su Gmail y contrasenia"
+        title="Inicie Sesión"
+        subtitle="Ingrese su Gmail y contraseña"
       />
-      <div className="rounded-t-xl bg-primary flex-grow pt-10 px-4">aca iria la pagina login</div>
+      <div className="rounded-t-xl bg-primary flex-grow pt-10 px-4">
+      <FormLogin/>
+      </div>
     </div>
   );
 }
